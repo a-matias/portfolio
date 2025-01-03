2015d0dac9811fbabcce46283404e0581f8d7af2
matias-andres <matias._@live.com>
2025-01-03 19:22:00 -0300

    modificaciones en el tipo de fuente de 'Hola, soy matias..'

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -6,7 +6,7 @@
         </div>
        
         <div class="w-full">
-          <h1 class="text-4xl font-bold mb-4">{{ displayedMessage }}<span class="blinking-cursor">|</span></h1>
+          <h1 class="nombre">{{ displayedMessage }}<span class="blinking-cursor">|</span></h1>
           <p class="text-lg text-gray-700">{{ description }}</p>
         </div>
         
@@ -48,6 +48,18 @@ typewriterEffect();
 
   
 <style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Cairo+Play&family=Diphylleia&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+  .nombre{
+    font-family: "Poppins", serif;
+    font-weight: 400;
+    font-size: 30px;
+    font-style: normal;
+    color: rgb(26, 63, 226);
+
+  }
+
   .blinking-cursor {
   display: inline-block;
   width: 10x;
