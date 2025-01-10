c7de74f266f3ef91186fdb8dc5eb605efde555c0
matias-andres <matias._@live.com>
2025-01-09 21:42:06 -0300

    mas mejoras

--- src/components/StackComponent.vue
+++ src/components/StackComponent.vue
@@ -50,6 +50,7 @@
     align-content: flex-start;
     width: 50vw;
     height: 100vh;
+    background-image: radial-gradient(73% 147%, #EADFDF 59%, #ECE2DF 100%), radial-gradient(91% 146%, rgba(255,255,255,0.50) 47%, rgba(0,0,0,0.50) 100%) !important;
     }
 
 
