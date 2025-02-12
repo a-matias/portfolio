89df1b8ffd0f535fe5ae2f995119cf4f5ddcf654
matias-andres <matias._@livecom>
2025-02-12 16:44:20 -0300

    cambio en vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+/*
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/portfolio/'
     : '/'
  }
+ */
+
+ module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/a-matias.github.io/portfolio/'  // Cambia a tu ruta real en GitHub Pages
+    : '/'
+}
+
