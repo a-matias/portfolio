654e1f2bf0d7301142a5a7eba5703efc7c966f19
matias-andres <matias._@livecom>
2025-02-12 20:14:44 -0300

    varias modificaciones

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  outputDir: 'public', // Esto hará que el build se genere en `public/`
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio/' // En producción, usar el subdirectorio portfolio
+    : '/'            // En desarrollo, la raíz
+}
