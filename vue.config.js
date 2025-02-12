4eb121be59aabe6a0edf680f22665b0963229f6c
matias-andres <matias._@livecom>
2025-02-12 17:48:10 -0300

    cambio en vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '//'  // En producción, cargar desde la raíz
+    ? '/portfolio/'  // En producción, cargar desde la raíz
     : '/'  // En desarrollo, también cargar desde la raíz
 }
